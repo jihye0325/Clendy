1d4d3b6e2dfdded8845cd2d67d1b531d7bf306a7
jihye0325 <hadocho1025@gmail.com>
2021-11-22 00:39:27 +0900

    jh

--- src/main/java/com/kh/clendy/mypage/model/service/MypageService.java
+++ src/main/java/com/kh/clendy/mypage/model/service/MypageService.java
@@ -3,8 +3,10 @@ package com.kh.clendy.mypage.model.service;
 import com.kh.clendy.member.model.vo.Member;
 
 public interface MypageService{
-	Member selectMember(String id);
+	Member selectMember(int user_no);
 	
-	int deleteMember(Member member);
-	
+	int modifyMember(Member m);
+
+	int deleteMember(int user_no);
+
 }
