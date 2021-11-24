329993d12f8cbcdacde45b0e57b9bb805e746e95
dandanmoon <answnsgh8691@gmail.com>
2021-11-25 08:01:54 +0900

    컨트롤러 수정

--- src/main/java/com/kh/clendy/mainpage/controller/MainpageController.java
+++ src/main/java/com/kh/clendy/mainpage/controller/MainpageController.java
@@ -2,13 +2,19 @@ package com.kh.clendy.mainpage.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/mainpage")
 public class MainpageController {
 	
-	@GetMapping("/mainpage")
-	public void toMainpage() {}
+	@GetMapping(value = {"/mainpage"})
+	   public String main() {
+	      return "mainpage/mainpage";
+	   }
 
+	@PostMapping(value="/")
+	   public String redirectMain() {
+	      return "redirect:/";
+	   }
 }
