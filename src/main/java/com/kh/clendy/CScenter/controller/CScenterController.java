2ad36462adadfbbf6cd3a8d65d73958affde0c67
SujeongJeong <88937730+SujeongJeong@users.noreply.github.com>
2021-11-24 14:57:43 +0900

    faq 수정,삭제 완료/ header 수정

--- src/main/java/com/kh/clendy/CScenter/controller/CScenterController.java
+++ src/main/java/com/kh/clendy/CScenter/controller/CScenterController.java
@@ -4,18 +4,20 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.kh.clendy.CScenter.model.service.BoardServiceImpl;
 import com.kh.clendy.CScenter.model.service.FAQServiceImpl;
 import com.kh.clendy.CScenter.model.vo.FAQ;
+import com.kh.clendy.member.model.vo.UserImpl;
 
 @Controller
 @RequestMapping("/CScenter")
@@ -32,6 +34,8 @@ public class CScenterController {
 		this.messageSource = messageSource;
 		this.boardService = boardService;
 	}
+	
+	/*-------------------------------- faq ----------------------------------------*/
 	
 	/* faq 전체 조회 */
 	@GetMapping("FAQ")
@@ -51,7 +55,11 @@ public class CScenterController {
 	/* faq 수정 */
 	@PostMapping("/FAQ/modify")
 	public String updateQna(FAQ newFaq, Model model) {
-		System.out.println(newFaq);
+		UserImpl user = (UserImpl)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		int user_no = user.getUser_no();
+		
+		newFaq.setUser_no(user_no);
+		
 		int result = faqService.updateFAQ(newFaq);
 		if(result>0) {
 			model.addAttribute("message", "수정완료");
@@ -62,13 +70,21 @@ public class CScenterController {
 		return "redirect:/CScenter/FAQ";
 	}
 	
+	/* faq 삭제 */
+	@PostMapping("/FAQ/delete")
+	@ResponseBody
+	public int deleteFAQ(@RequestParam int q_no){
+		return faqService.deleteFAQ(q_no);
+	}
+	
 	/* faq 등록페이지 */
 	@GetMapping("insertFAQ")
 	public void toInsertFAQ() {}
 	
 	@PostMapping("/FAQ/insert")
 	public String insertFAQ(FAQ newFAQ, Model model) {
-		newFAQ.setUser_no(1);
+		UserImpl user = (UserImpl)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		int user_no = user.getUser_no();
 		
 		int result = faqService.insertFAQ(newFAQ);
 		if(result>0) {
@@ -80,7 +96,9 @@ public class CScenterController {
 		return "redirect:/CScenter/FAQ";
 	}
 	
+	/*------------------ 공지사항 ----------------*/
 	
+	/**/
 	@GetMapping("/board")
 	public void toBoard() {}
 	
