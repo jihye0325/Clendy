ad2afd98c16bdc0b800ca80c3632bda4fc9ecdfe
SujeongJeong <88937730+SujeongJeong@users.noreply.github.com>
2021-11-25 10:08:19 +0900

    백업

---
+++ src/main/java/com/kh/clendy/joinStore/model/service/JoinStoreService.java
@@ -0,0 +1,9 @@
+package com.kh.clendy.joinStore.model.service;
+
+import com.kh.clendy.joinStore.model.vo.ApplyStore;
+
+public interface JoinStoreService {
+	
+	/* 입점신청 */
+	int applyStore(ApplyStore newStore);
+}
