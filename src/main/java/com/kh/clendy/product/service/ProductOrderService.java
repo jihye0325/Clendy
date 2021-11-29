6687ce872a36e5ee0555a734a7d7935cb0fc02cd
godot4027 <godot4027@nate.com>
2021-11-29 19:58:02 +0900

    상품 주문 페이지 결제 금액 확인 제외 완료

--- src/main/java/com/kh/clendy/product/service/ProductOrderService.java
+++ src/main/java/com/kh/clendy/product/service/ProductOrderService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Map;
 
 import com.kh.clendy.member.model.vo.Member;
+import com.kh.clendy.mypage.model.vo.Coupon;
 import com.kh.clendy.product.model.vo.Order;
 import com.kh.clendy.product.model.vo.ProductCart;
 
@@ -14,5 +15,10 @@ public interface ProductOrderService {
 
 	// 주문내역
 	Map<String, List<Order>> orderInfoSelect(Map<String, List<ProductCart>> cartMap);
+
+	// 포인트 조회
+	int orderPointSelect(int userNo);
+
+	List<Coupon> orderCouponSelectList(int userNo);
 
 }
