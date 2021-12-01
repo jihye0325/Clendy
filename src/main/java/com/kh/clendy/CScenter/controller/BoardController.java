2753d4fb0d72bd471ed13c8314ae99bb40cef364
SujeongJeong <88937730+SujeongJeong@users.noreply.github.com>
2021-12-02 03:28:30 +0900

    스마트에디터 삭제/ 1:1문의 구현완료, 공지사항 구현완료, 적립금 구현완료

--- src/main/java/com/kh/clendy/CScenter/controller/BoardController.java
+++ src/main/java/com/kh/clendy/CScenter/controller/BoardController.java
@@ -2,17 +2,20 @@ package com.kh.clendy.CScenter.controller;
 
 import java.util.Map;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+
 import com.kh.clendy.CScenter.model.service.BoardService;
 import com.kh.clendy.CScenter.model.vo.Board;
+import com.kh.clendy.member.model.vo.UserImpl;
 
 @Controller
 @RequestMapping("/CScenter/board")
@@ -47,7 +50,7 @@ public class BoardController {
 	
 	/* 공지사항 삭제 */
 	@GetMapping("/delete")
-	public String deleteBoard(@RequestParam int b_no, RedirectAttributes redirectAttr, Model model) {
+	public String deleteBoard(@RequestParam int b_no, RedirectAttributes redirectAttr) {
 		
 		int result = boardService.deleteBoard(b_no);
 		String resultPage;
@@ -56,10 +59,9 @@ public class BoardController {
 			redirectAttr.addFlashAttribute("msg", "공지사항이 삭제되었습니다.");
 			resultPage = "redirect:/CScenter/board";
 		}else {
-			Board resultBoard = boardService.selectOneBoard(b_no);
-			model.addAttribute("board", resultBoard);
-			model.addAttribute("msg", "공지사항 삭제에 실패하였습니다.");
-			resultPage = "/CScenter/detailBoard";
+			
+			redirectAttr.addFlashAttribute("msg", "공지사항 삭제에 실패하였습니다.");
+			resultPage = "redirect:/CScenter/board/detail?b_no="+b_no;
 		}
 		
 		return resultPage;
@@ -71,23 +73,52 @@ public class BoardController {
 		return "/CScenter/insertBoard";
 	}
 	
-	/* 싱글이미지 등록 */
-/*	@RequestMapping(value="/singleImageUploader.do")
-		public String singleImageUploader(HttpServletRequest req, 
-				SmartEditorSingle smartEditorSingle) throws UnsupportedEncodingException{
-		String callback = smartEditorSingle.getCallback();
-		String callback_func = smartEditorSingle.getCallback_func();
-		String file_result ="";
-		String result ="";
+	/* 공지사항 등록 */
+	@PostMapping("/insert")
+	public String insertBoard(Board newBoard, RedirectAttributes redirectAttr) {
+		UserImpl user = (UserImpl)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 		
-		MultipartFile multiFile = smartEditorSingle.getfiledata();
-		try {
-			if(multiFile != null && multiFile.getSize() > 0 && 
-					StringUtils.is(multiFile.getName())) {
-				
-			}
-		} 
-	}*/
+		newBoard.setUser_no(user.getUser_no());
+		
+		int result = boardService.insertBoard(newBoard);
+		String resultPage;
+		
+		if(result >0) {
+			redirectAttr.addFlashAttribute("msg","공지사항이 등록되었습니다.");
+			resultPage = "redirect:/CScenter/board/delete?b_no="+ newBoard.getB_no();
+		}else {
+			redirectAttr.addFlashAttribute("msg","공지사항 등록에 실패하였습니다.");
+			resultPage = "redirect:/CScenter/board";
+		}
+		
+		return resultPage;
+	}
+	
+	/* 공지사항 수정 */
+	@GetMapping("/modify")
+	public String toUpdateBoard(int b_no, Model model) {
+		
+		Board result = boardService.selectOneBoard(b_no);
+		
+		model.addAttribute("board", result);	
+	
+		return "/CScenter/modifyBoard";
+	}
+	
+	@PostMapping("/modify")
+	public String updateBoard(Board updateBoard, RedirectAttributes redirectAttr) {
+		
+		int result = boardService.updateBoard(updateBoard);
+		
+		if(result >0) {
+			redirectAttr.addFlashAttribute("msg","공지사항이 수정되었습니다.");
+		}else {
+			redirectAttr.addFlashAttribute("msg","공지사항 수정에 실패하였습니다.");
+		}
+		
+		return "redirect:/CScenter/board/delete?b_no="+ updateBoard.getB_no();
+		
+	}
 	
 	
 	
@@ -96,5 +127,7 @@ public class BoardController {
 	
 	
 	
-
+	
+	
+	
 }
